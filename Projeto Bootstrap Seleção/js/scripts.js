0ee0d1cb55be3ca385f1c816b12dbad7b41a4092
João Mariano <84255348+JoonMarion@users.noreply.github.com>
2021-11-24 19:52:46 -0300

    Add files via upload

---
+++ Projeto Bootstrap Seleção/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    // setTimeout serve para carregar primeiro as imagens
+    setTimeout(function () {
+        $('#apply-area').parallax({ imageSrc: '../img/anilhaR.jpg' });
+    }, 200);
+
+});
+
